4f9b5d9a20b9d358b5d2f82604a59956eba898c3
mypjb <280417314@qq.com>
2024-03-14 21:23:39 +0800

    [sln] upgrade package

--- test/Plugins/SAE.CommonLibrary.Plugin.FrontEnd/Startup.cs
+++ test/Plugins/SAE.CommonLibrary.Plugin.FrontEnd/Startup.cs
@@ -1,10 +1,6 @@
 using SAE.CommonLibrary.Plugin.AspNetCore;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using SAE.CommonLibrary.Plugin.Constant;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 
 namespace SAE.CommonLibrary.Plugin.FrontEnd
 {
